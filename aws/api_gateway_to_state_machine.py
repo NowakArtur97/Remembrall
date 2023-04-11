80df49ca33e961dd97d53db20a394e5452dc5736
NowakArtur97 <nowart97@gmail.com>
2023-04-11 08:48:41 +0200

    Create State Machine resource

--- aws/api_gateway_to_state_machine.py
+++ aws/api_gateway_to_state_machine.py
@@ -1,12 +1,17 @@
 import boto3, json, os
 
+sm = boto3.client('stepfunctions')
+STATE_MACHINE_ARN = os.environ["STATE_MACHINE_ARN"]
+
 def validateData(data):
     checks = []
     checks.append('message' in data)
     checks.append(type(data['timeDelay']) == int)
-    checks.append('option' in data)
-    if data['option'] in ['email', 'both']:
+    checks.append('notificationType' in data)
+    if data['notificationType'] in ['email', 'both']:
         checks.append(data['email'] != "")
+    if data['notificationType'] in ['sms', 'both']:
+        checks.append(data['phoneNumber'] != "")
     return checks
 
 def lambda_handler(event, context):
@@ -22,6 +27,7 @@ def lambda_handler(event, context):
             "body": json.dumps( { "Status": "Success", "Reason": "Input failed validation" } )
         }
     else:
+        sm.start_execution(stateMachineArn=STATE_MACHINE_ARN, input=json.dumps(data))
         response = {
             "statusCode": 200,
             "body": json.dumps( {"Status": "Success"} )
